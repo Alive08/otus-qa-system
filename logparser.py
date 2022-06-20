3e2f94e6c431280d6b8b942d1cd75aaf60274e2f
“Vadim <diginet@mail.ru>
2022-06-20 21:37:13 +0700

    tweaked default args

--- logparser.py
+++ logparser.py
@@ -1,9 +1,9 @@
-from pathlib import Path
 import json
 from argparse import ArgumentParser
-import pandas as pd
-import pandas.errors as errors
+from pathlib import Path
+
 import numpy as np
+import pandas as pd
 
 
 # Формат записи в файле лога (combined):
@@ -25,13 +25,14 @@ cols = {'host': str, 'l': str, 'user': str, 'timestamp': str, 'tz': str, 'reques
 
 parser = ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument('--file')
-group.add_argument('--dir')
-parser.add_argument('--to-file', nargs='?', default='access_report.json')
-parser.add_argument('--pattern', default='access*.log')
+group.add_argument('--file', nargs='?', const='access.log', default=None)
+group.add_argument('--dir', nargs='?', const='./', default=None)
+parser.add_argument('--to-file', nargs='?', const='access_stats.json', default=None)
+parser.add_argument('--pattern', default='access*.log*')
 args = parser.parse_args()
 
-def parse_args(parser):
+
+def get_files():
     """Parse arguments"""
     file = args.file
     dir = args.dir
@@ -43,21 +44,28 @@ def parse_args(parser):
     else:
         print("No file or directory was given")
         exit(1)
-    
+
     return files
 
 
 def load_data(files):
     """Load data from given files"""
     try:
-        df = pd.concat((pd.read_csv(f, delim_whitespace=True, names=cols.keys(),
-                                    dtype=cols, keep_default_na=False, na_values=["-", ""]) for f in files)).drop(columns='l')
+        df = pd.concat((pd.read_csv(f,
+                                    delim_whitespace=True,
+                                    quotechar='"',
+                                    escapechar='\\',
+                                    names=cols.keys(),
+                                    dtype=cols,
+                                    na_values=["-", ""]) for f in files)).drop(columns='l')
     except Exception as e:
         print(e)
         exit(1)
-    df["timestamp"] = df["timestamp"].str.lstrip("[") + " " + df.pop("tz").str.rstrip("]")
+
+    df["timestamp"] = df["timestamp"].str.lstrip(
+        "[") + " " + df.pop("tz").str.rstrip("]")
     df[['method', 'url']] = df.pop('request').str.split(' ', 1, expand=True)
-    
+
     return df
 
 
@@ -65,21 +73,25 @@ def prepare_report(df: pd.DataFrame):
     rep = {"methods": None,
            "top_hosts": None,
            "top_long_requests": None
-    }
+           }
 
     reqs = df['method'].value_counts(dropna=True, ascending=False).to_dict()
     rep['methods'] = reqs
     rep['methods']['TOTAL'] = sum(reqs.values())
 
-    rep['top_hosts'] = df['host'].value_counts(dropna=True, ascending=False).head(3).to_dict()
+    rep['top_hosts'] = df['host'].value_counts(
+        dropna=True, ascending=False).head(3).to_dict()
 
-    long_reqs = df.sort_values(by=['mils'], ascending=False).drop(columns=['user', 'status', 'bytes', 'referer', 'ua']).head(3)
-    rep['top_long_requests'] = long_reqs.astype(object).replace(np.nan, 'None').to_dict(orient='records')
+    long_reqs = df.sort_values(by=['mils'], ascending=False).drop(
+        columns=['user', 'status', 'bytes', 'referer', 'ua']).head(3)
+    rep['top_long_requests'] = long_reqs.astype(object).replace(
+        np.nan, 'Null').to_dict(orient='records')
 
     return rep
 
 
 def out_to_console(rep):
+    print(f"Access log(s) analysis summary:")
     print(json.dumps(rep, indent=4))
 
 
@@ -91,9 +103,12 @@ def out_to_file(rep, file):
         print(e)
         exit(1)
 
+
 def main():
-    
-    rep = prepare_report(load_data(parse_args(parser)))
+
+    logs = get_files()
+
+    rep = prepare_report(load_data(logs))
 
     out_to_console(rep)
 
